b24c42c129426fa074e8c5f06dbda8a2212edd2f
MariuszKoz <mariusz5637wa@gmail.com>
2022-11-03 19:29:57 +0100

    Admin, SuperAdmin and Supervisor role to User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_03_175925) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_03_182602) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -169,6 +169,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_03_175925) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false, null: false
+    t.boolean "superadmin", default: false, null: false
+    t.boolean "supervisor", default: true, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["store_id"], name: "index_users_on_store_id"
